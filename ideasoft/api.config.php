43c97249a51d0d62277bde412829d015cb28da2b
Mert Özkaymak <101987240+mertozkaymak@users.noreply.github.com>
2022-03-21 03:27:03 +0300

    Update api.config.php

--- ideasoft/api.config.php
+++ ideasoft/api.config.php
@@ -2,15 +2,15 @@
 
     //Database Info
     define("HOST", "localhost");
-    define("DBNAME", "formoda");
-    define("USER", "formoda");
-    define("PASS", "Digital1357*c");
+    define("DBNAME", "***");
+    define("USER", "***");
+    define("PASS", "***");
 
     //Shop Info
-    define("URL", "https://www.formoda.com.tr");
-    define("CLIENTID", "7_1qwi65z4fqlccsc0s408gcowwo88ocww4cww4csk04g0880o44");
-    define("SECRETID", "x7asn5azzm88ckg80gc8gco8g0oco0g4osk8o04g88ws8gwgg");
-    define("REDIRECT", "https://dev.digitalfikirler.com/formoda/controller.php");
-    define("ACCESS", URL . "/admin/user/auth?client_id=" . CLIENTID . "&response_type=code&state=d12b2a61b81a52be4a4cac7e610f62f8&redirect_uri=" . REDIRECT);
+    define("URL", "***");
+    define("CLIENTID", "***");
+    define("SECRETID", "***");
+    define("REDIRECT", "***");
+    define("ACCESS", URL . "/admin/user/auth?client_id=" . CLIENTID . "&response_type=code&state=***&redirect_uri=" . REDIRECT);
 
 ?>
